f80d830b76b384efa809eb672e529fa1856d2a55
Peter Vliet <petervliet@Peters-MacBook-Air.local>
2015-03-16 16:59:48 -0600

    user can select state from drop down to look up reps

--- app/models/data_fetcher.rb
+++ app/models/data_fetcher.rb
@@ -8,11 +8,11 @@ class DataFetcher
      end
    end
 
- def rep_info
+ def rep_info(state)
    response = @rep_search.get do |req|
      req.url "/api/"
      req.params['method'] = "getLegislators"
-     req.params['id'] = "NJ"
+     req.params['id'] = state
      req.params['apikey'] = ENV["API_KEY"]
      req.params['output'] = 'json'
    end

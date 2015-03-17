8f0b7d12496105d669b6873d68bf67a616ed9f09
Peter Vliet <petervliet@Peters-MacBook-Air.local>
2015-03-16 18:21:02 -0600

    improve markup on home index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,14 @@ class HomeController < ApplicationController
 def index
   @df = DataFetcher.new
   @reps = []
+
   if params[:state]
     @rep_info_results = @df.rep_info(params[:state])
-    # @name = @rep_info_results["response"]["legislator"][0]["@attributes"]["firstlast"]
     @rep_info_results["response"]["legislator"].each_with_index do |rep, index|
       @reps << @rep_info_results["response"]["legislator"][index]["@attributes"]["firstlast"]
     end
-    @rep_info_results.map {|rep| "rep"}
   end
+
 end
 
 end

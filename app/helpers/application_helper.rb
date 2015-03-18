f8fde2ec87e33c6b087cfd83c12197bb3e1b034e
Josh Qberry <joshqberry@gmail.com>
2015-03-18 10:12:41 -0600

    Direct logged-in user to dashboard; direct visitor to intro page; add log-in and log-out flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def current_user
+        if session[:user_id]
+          @current_user = @current_user || User.find_by(twitter_id: session[:user_id])
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :reps, only: [:index, :show], param: :cid
 
-  root 'reps#index'
+  root 'landing#dashboard'
 
   get 'auth/twitter/callback' => 'sessions#create'
 
@@ -11,5 +11,9 @@ Rails.application.routes.draw do
   resources :users
 
   get '/logout' => 'sessions#destroy'
+
+  get '/dashboard' => 'landing#dashboard'
+
+  get '/visitors' => 'landing#visitors'
 
 end

d2e3a3ae51c2a2d48458fea84c0bb9533270e608
Wesley Merkel <ooesili@gmail.com>
2015-03-19 15:56:11 -0600

    Cache DataFetcher calls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  # use simple memory caching
+  config.cache_store = :memory_store
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 

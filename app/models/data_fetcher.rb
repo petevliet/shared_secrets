dfac8fc20f35906e053c0c3c1c8d541bb952a844
Wesley Merkel <ooesili@gmail.com>
2015-03-19 15:56:11 -0600

    Cache DataFetcher calls

--- app/models/data_fetcher.rb
+++ app/models/data_fetcher.rb
@@ -9,50 +9,58 @@ class DataFetcher
   end
 
  def state_info(state)
-   response = @rep_search.get do |req|
-     req.url "/api/"
-     req.params['method'] = "getLegislators"
-     req.params['id'] = state
-     req.params['apikey'] = ENV["API_KEY"]
-     req.params['output'] = 'json'
+   Rails.cache.fetch("DataFetcher#state_info(#{state})", expires_in: 12.hours) do
+     response = @rep_search.get do |req|
+       req.url "/api/"
+       req.params['method'] = "getLegislators"
+       req.params['id'] = state
+       req.params['apikey'] = ENV["API_KEY"]
+       req.params['output'] = 'json'
+     end
+     JSON.parse(response.body)
    end
-   JSON.parse(response.body)
  end
   
  def rep_info(cid)
-   response = @rep_search.get do |req|
-     req.url "/api/"
-     req.params['method'] = "candSummary"
-     req.params['cid'] = cid
-     req.params['apikey'] = ENV["API_KEY"]
-     req.params['output'] = 'json'
-     req.params['cycle'] = '2014'
+   Rails.cache.fetch("DataFetcher#rep_info(#{cid})", expires_in: 12.hours) do
+     response = @rep_search.get do |req|
+       req.url "/api/"
+       req.params['method'] = "candSummary"
+       req.params['cid'] = cid
+       req.params['apikey'] = ENV["API_KEY"]
+       req.params['output'] = 'json'
+       req.params['cycle'] = '2014'
+     end
+     JSON.parse(response.body)
    end
-   JSON.parse(response.body)
  end
 
  def contrib_info(cid)
-   response = @rep_search.get do |req|
-     req.url "/api/"
-     req.params['method'] = "candContrib"
-     req.params['cid'] = cid
-     req.params['apikey'] = ENV["API_KEY"]
-     req.params['output'] = 'json'
-     req.params['cycle'] = '2014'
+   Rails.cache.fetch("DataFetcher#contrib_info(#{cid})", expires_in: 12.hours) do
+     response = @rep_search.get do |req|
+       req.url "/api/"
+       req.params['method'] = "candContrib"
+       req.params['cid'] = cid
+       req.params['apikey'] = ENV["API_KEY"]
+       req.params['output'] = 'json'
+       req.params['cycle'] = '2014'
+     end
+     JSON.parse(response.body)
    end
-   JSON.parse(response.body)
  end
 
  def industry_info(cid)
-   response = @rep_search.get do |req|
-     req.url "/api/"
-     req.params['method'] = "candIndustry"
-     req.params['cid'] = cid
-     req.params['apikey'] = ENV["API_KEY"]
-     req.params['output'] = 'json'
-     req.params['cycle'] = '2014'
+   Rails.cache.fetch("DataFetcher#industry_info(#{cid})", expires_in: 12.hours) do
+     response = @rep_search.get do |req|
+       req.url "/api/"
+       req.params['method'] = "candIndustry"
+       req.params['cid'] = cid
+       req.params['apikey'] = ENV["API_KEY"]
+       req.params['output'] = 'json'
+       req.params['cycle'] = '2014'
+     end
+     JSON.parse(response.body)
    end
-   JSON.parse(response.body)
  end
 
 end
